db6fd143cb55e294b437d7a0d96ff34a4e9c713d
leonardoviada <leo@revodigital.it>
2020-11-29 15:09:57 +0100

    directories update and FactoryMethod

---
+++ README.md
@@ -0,0 +1,33 @@
+# TS Design Patterns
+Leonardo Viada - 2020
+
+### Creazionali
+- Factory method [x]
+- Singoletto
+- Prototype
+- Builder
+- Abstract factory
+
+### Strutturali
+- Composite
+- Proxy
+- Facade
+- Flyweight
+- Adapter
+- Decorator
+- Bridge
+- Twins
+
+### Comportamentali
+- Observer
+- Memento
+- Template
+- Null Object
+- State
+- Chain of responsability
+- Iterator
+- Mediator 
+- Command 
+- Interpreter 
+- Strategy
+- Visitor
---
+++ creazionali/FactoryMethod.ts
@@ -0,0 +1,31 @@
+abstract class Prodotto {
+  protected constructor(public prezzo: number) {}
+
+  abstract vendi(): void;
+};
+
+abstract class Fabbrica {
+  abstract produci(params: Object): Prodotto;
+}
+
+/* --- */
+
+class Panettone extends Prodotto {
+  constructor(public peso: number, public senzaCanditi: boolean, prezzo: number) {super(prezzo);}
+
+  vendi(): void {
+    console.log(`Panettone venduto per €${ this.prezzo }`);
+  }
+}
+
+class FabbricaDiPanettoni extends Fabbrica {
+  produci = ({ peso, senzaCanditi, prezzo }): Panettone => {
+    console.log('Ho prodotto un panettone');
+    return new Panettone(peso, senzaCanditi, prezzo);
+  };
+}
+
+const maina = new FabbricaDiPanettoni();
+const panettone = maina.produci({ peso: 1, senzaCanditi: false, prezzo: 2.99 });
+panettone.vendi();
+
